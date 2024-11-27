3440f32f5846699af47c62b574f862d3783a1c5d
sametkzdl <sametkazdal_53@outlook.com>
2024-11-27 22:40:30 +0300

    vite değişikliği

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // Build çıktısının dizini
+  },
+  server: {
+    port: 8080, // Azure’un dinlediği porta uyumlu
+  },
+});
