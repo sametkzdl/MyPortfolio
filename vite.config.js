4914211404f3295a513436965528b4c20c23a07e
sametkzdl <sametkazdal_53@outlook.com>
2024-12-01 00:20:15 +0300

    vite.config düzenlendi

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/MyPortfolio/",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name].[hash].js",
+        chunkFileNames: "assets/[name].[hash].js",
+        assetFileNames: "assets/[name].[hash].[ext]",
+      },
+    },
+  },
 });
