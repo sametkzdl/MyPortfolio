45d677ef6609b4d714af0caf81245de44f4a87d5
sametkzdl <sametkazdal_53@outlook.com>
2025-07-17 14:52:36 +0300

    page

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import MainLayout from "./layout/main.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/MyPortfolio">
+  <HashRouter basename="/MyPortfolio">
     <MainLayout>
       <App />
     </MainLayout>
-  </BrowserRouter>
+  </HashRouter>
 );
